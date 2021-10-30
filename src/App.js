e8df24022c15d5a7872b5db053fe12a91197f962
conqryash007 <guptayash200010@gmail.com>
2021-10-30 08:53:48 +0530

    completed ethereum setup

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import './App.css';
-import React from 'react';
-import { Route , Switch } from 'react-router-dom';
-import Home from './Home';
-import SchoolForm from './SchoolForm';
-import SchoolList from './SchoolList';
-import StudentList from './StudentList';
-import Tenth from './Tenth';
-import Twelfth from './Twelfth';
-import Dashboard from './Dashboard';
-import StudentEnrollForm from './StudentEnrollForm';
-import TenthEnrollForm from './StudentEnrollForm';
-import TwelfthEnrollForm from './StudentEnrollForm';
+import "./App.css";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import SchoolForm from "./SchoolForm";
+import SchoolList from "./SchoolList";
+import StudentList from "./StudentList";
+import Tenth from "./Tenth";
+import Twelfth from "./Twelfth";
+import Dashboard from "./Dashboard";
+import StudentEnrollForm from "./StudentEnrollForm";
+import TenthEnrollForm from "./StudentEnrollForm";
+import TwelfthEnrollForm from "./StudentEnrollForm";
 
-
-
-
-function App() {
-
-  
-  return (
-    <Switch>
-      <Route  exact path = '/' component = {Home} />
-      <Route  path = '/schoolform' component = {SchoolForm} />
-      <Route  path = '/schoollist' component = {SchoolList} />
-      <Route  path = '/studentlist' component = {StudentList}/>
-      <Route  path = '/tenth' component = {Tenth} />
-      <Route  path = '/twelfth' component = {Twelfth}/>
-      <Route   path= '/dashboard' component = {Dashboard}/>
-      <Route  path = '/studentenrollform' component = {StudentEnrollForm} />
-      <Route  path = '/tenthenrollform' component = {TenthEnrollForm} />
-      <Route  path = '/twelfthenrollform' component = {TwelfthEnrollForm} />
-    </Switch>
-
-  );
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/schoolform" component={SchoolForm} />
+        <Route path="/schoollist" component={SchoolList} />
+        <Route path="/studentlist" component={StudentList} />
+        <Route path="/tenth" component={Tenth} />
+        <Route path="/twelfth" component={Twelfth} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/studentenrollform" component={StudentEnrollForm} />
+        <Route path="/tenthenrollform" component={TenthEnrollForm} />
+        <Route path="/twelfthenrollform" component={TwelfthEnrollForm} />
+      </Switch>
+    );
+  }
 }
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,45 +1,60 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import { CssBaseline } from '@material-ui/core';
-import Header from './Header';
-import Enroll from './Enroll';
-import School from './School';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+import Header from "./Header";
+import Enroll from "./Enroll";
+import School from "./School";
 
-const useStyles = makeStyles ((theme)=>({
-   
-    root : {
-      minHeight : '100vh' ,
-      backgroundImage : `url(${process.env.PUBLIC_URL + "/asset/a.jpg " })`  ,
-      backgroundRepeat : 'no-repeat' ,
-      backgroundSize : 'cover',
- 
-    } ,
- 
-    draw : {
-       justifyContent : "center" ,
-       alignItems : "center" ,
-       minHeight : '100vh',
-       display : 'flex' ,
-    },
- 
- 
- })) ;
+import getweb3 from "./ethereum/getweb3";
+import compiledCreateSchool from "./ethereum/build/CreateSchool.json";
 
-const Home = () => {
-    
-    const classes = useStyles();  
+const useStyles = (theme) => ({
+  root: {
+    minHeight: "100vh",
+    backgroundImage: `url(${process.env.PUBLIC_URL + "/asset/a.jpg "})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+
+  draw: {
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
+    display: "flex",
+  },
+});
+
+class Home extends Component {
+  state = {
+    web3: null,
+    accounts: [],
+    createSchool: null,
+  };
+  componentDidMount = async () => {
+    const w = await getweb3();
+    this.state.web3 = w;
+    this.state.accounts = await w.eth.getAccounts();
+    this.state.createSchool = new w.eth.Contract(
+      compiledCreateSchool.abi,
+      "0x67b1a6400E0E1735Ad8876cE28fE09150fd3E1d7"
+    );
+    console.log(this.state);
+  };
+  render() {
+    const { classes } = this.props;
     return (
-        <div>
-        <div className = {classes.root} >
-        <CssBaseline/>
-        <Header/>
-        <div className = {classes.draw} >
-        <Enroll/>
-        <School/>
+      <div>
+        <div className={classes.root}>
+          <CssBaseline />
+          <Header />
+          <div className={classes.draw}>
+            <Enroll />
+            <School />
+          </div>
         </div>
-        </div>
-        </div>
-    )
+      </div>
+    );
+  }
 }
 
-export default Home ;
+export default withStyles(useStyles)(Home);
